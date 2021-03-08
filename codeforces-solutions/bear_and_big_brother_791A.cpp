76f43523b543b86c8054c8dbdec7645db9afd93a
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-08 13:48:59 +0600

    Create bear_and_big_brother_791A.cpp

---
+++ codeforces-solutions/bear_and_big_brother_791A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b,i;
+    cin >> a >> b;
+    for (i=1;;i++)
+    {
+        a = a*3;
+        b = b*2;
+        if(a>b)
+        {
+            break;
+        }
+    }
+    cout << i <<endl;
+    return 0;
+}
