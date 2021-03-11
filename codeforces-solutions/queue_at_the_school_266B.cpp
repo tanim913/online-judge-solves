cd66618389333765cb82d2fad11cee2659b0f7ea
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-11 22:06:14 +0600

    Create queue_at_the_school_266B.cpp

---
+++ codeforces-solutions/queue_at_the_school_266B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t;
+    string s;
+
+    cin >> n >> t >> s;
+
+    for (int j= 1 ; j <= t; j++)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if(s[i] == 'G' && s[i-1] == 'B')
+            {
+                s[i] = 'B';
+                s[i-1] = 'G';
+                i++;
+            }
+        }
+    }
+    cout  << s << endl;
+    return 0;
+}
