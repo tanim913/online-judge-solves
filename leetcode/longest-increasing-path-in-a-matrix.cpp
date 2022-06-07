6fa5521518d0fd3b540676b34c099620fd0b8757
Tanim <42853370+tanim913@users.noreply.github.com>
2022-06-07 17:54:53 +0600

    Update longest-increasing-path-in-a-matrix.cpp

--- leetcode/longest-increasing-path-in-a-matrix.cpp
+++ leetcode/longest-increasing-path-in-a-matrix.cpp
@@ -2,13 +2,12 @@ class Solution
 {
     int rows, cols;
     vector<vector<int>> mem;
-    int directions[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    vector <pair <int,int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
     bool valid(int i, int j)
     {
         return (i >= 0 && i < rows && j >= 0 && j < cols);
     }
-
     int long_path(vector<vector<int>> &matrix, int i, int j)
     {
         if (mem[i][j])
@@ -16,8 +15,8 @@ class Solution
         int max_dir_path = 0;
         for (auto dir : directions)
         {
-            int dir_i = i + dir[0];
-            int dir_j = j + dir[1];
+            int dir_i = i + dir.first;
+            int dir_j = j + dir.second;
 
             if (valid(dir_i, dir_j) && matrix[i][j] < matrix[dir_i][dir_j])
             {
@@ -27,7 +26,6 @@ class Solution
         mem[i][j] = 1 + max_dir_path;
         return mem[i][j];
     }
-
 public:
     int longestIncreasingPath(vector<vector<int>> &matrix)
     {
