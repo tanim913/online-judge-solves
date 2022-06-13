06eecf46bd6af345e44074a7272237cf6f082a23
Tanim <42853370+tanim913@users.noreply.github.com>
2022-06-14 04:59:46 +0600

    ex.cpp added

---
+++ uri-online-judge-solutions/ex.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i =0; i<=20; i++)
+    {
+
+        (-1* abs(10-i)) == 0 ? cout << endl : cout << (-1* abs(10-i)) <<endl;
+
+    }
+    return 0;
+}
